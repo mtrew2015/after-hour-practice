de09d98eed594f4488a633ff39dfe3841c35bb20
Michael Trew <mtrew2015@gmail.com>
2020-01-28 22:33:31 -0500

    setup initial structure and cleaned up files and added dependencies

---
+++ extra-practice/src/Components/Form/Form.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Form.scss';
+
+export default function Form() {
+    return (
+        <div>
+           <p>Form Goes Here</p>
+        </div>
+    )
+}
---
+++ extra-practice/src/Components/Landing Page/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Landing.scss';
+import Form from '../Form/Form';
+
+ function Landing() {
+    return (
+        <div>
+            <Form/>
+        </div>
+    )
+}
+
+export default Landing
